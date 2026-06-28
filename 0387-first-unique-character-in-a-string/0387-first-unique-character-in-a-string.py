9acdcedb3c8cbffdeedd13625c354653fba6c8df
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-06-28 09:11:19 +0530

    Time: 71 ms (60.72%), Space: 19.5 MB (87.72%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,14 +1,23 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         # in brute force method 
-        for iEle in range(len(s)):
-            # print(s)
-            news = s.replace(s[iEle],"",1)
-            # print(news)
-            # print(s[iEle])
-            if s[iEle] not in news:
-                return iEle
+        # for iEle in range(len(s)):
+        #     # print(s)
+        #     news = s.replace(s[iEle],"",1)
+        #     # print(news)
+        #     # print(s[iEle])
+        #     if s[iEle] not in news:
+        #         return iEle
         
+        # return -1
+
+        # optimised way ti fix 
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch,0)+1
+        for ch in range(len(s)):
+            if freq[s[ch]]  ==1:
+                return ch
         return -1
 
         
