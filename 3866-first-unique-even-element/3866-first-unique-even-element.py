3ed0a1d9e2ba729a69050b995dfc78fb9f40e08b
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-06-28 09:38:23 +0530

    Time: 3 ms (42.49%), Space: 19.2 MB (90.57%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.py
+++ 3866-first-unique-even-element/3866-first-unique-even-element.py
@@ -1,14 +1,21 @@
 class Solution:
     def firstUniqueEven(self, nums: list[int]) -> int:
         # let first bind the even number dic 
-        dictCollection = {}
-        for ch in range(len(nums)):
-            if(nums[ch]%2 == 0):
-                dictCollection[nums[ch]] = dictCollection.get(nums[ch],0)+1
-        # print(dictCollection.keys())
+        # dictCollection = {}
+        # for ch in range(len(nums)):
+        #     if(nums[ch]%2 == 0):
+        #         dictCollection[nums[ch]] = dictCollection.get(nums[ch],0)+1
+        # # print(dictCollection.keys())
 
-        for iii in dictCollection.keys():
-            print(iii)
-            if(dictCollection[iii] == 1):
-                return iii
+        # for iii in dictCollection.keys():
+        #     print(iii)
+        #     if(dictCollection[iii] == 1):
+        #         return iii
+        # return -1
+        #
+        # best way 
+        counter  = Counter(nums)
+        for key , val in counter.items():
+            if(key % 2 == 0 and val == 1):
+                return key
         return -1
