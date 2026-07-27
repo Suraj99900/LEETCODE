c87849297cf14c312ad3e0bd37870903dcc66a99
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-07-27 07:44:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (77.28%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,34 @@
+class Solution:
+    dictMpped = {
+            2:"abc",
+            3:"def",
+            4:"ghi",
+            5:"jkl",
+            6:"mno",
+            7:"pqrs",
+            8:"tuv",
+            9:"wxyz"
+        }
+    def letterCombinations(self, digits: str) -> List[str]:
+        aResultList = []
+        mappedString = ''
+        bOneRoundTrack=True
+        if not digits:
+            return []
+        mappedString = self.dictMpped[int(digits[0])]
+        for jjj in range(len(mappedString)):
+
+            aResultList.append(mappedString[jjj])
+        # print(aResultList)
+        
+        
+        for num in range(1,len(digits)):
+            newList = []
+            for old in aResultList:
+                mappedString = self.dictMpped[int(digits[int(num)])]
+                for char in mappedString:
+                    newList.append(old+char)
+            aResultList = newList
+        return aResultList
+        
+        
