2e0b908f1b64d67b44f2cdbc599edb14eaf345b7
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-08-02 18:53:02 +0530

    Time: 19 ms (6.6%), Space: 21.2 MB (18.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,21 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        # dictData = {}
-        # for i in range(len(nums)):
-        #     if nums[i] not in dictData.keys():
-        #         dictData[nums[i]] = 1
-        #     else:
-        #         dictData[nums[i]] +=1
-        # return max(dictData,key=dictData.get) 
-        count =0 
-        candidate = None
-        for i in nums:
-            if (count == 0):
-                candidate = i 
-           
-            if(i == candidate):
-                count+=1
+        dictData = {}
+        for i in range(len(nums)):
+            if nums[i] not in dictData.keys():
+                dictData[nums[i]] = 1
             else:
-                count-=1
-        return candidate
+                dictData[nums[i]] +=1
+        return max(dictData,key=dictData.get) 
+        # count =0 
+        # candidate = None
+        # for i in nums:
+        #     if (count == 0):
+        #         candidate = i 
+           
+        #     if(i == candidate):
+        #         count+=1
+        #     else:
+        #         count-=1
+        # return candidate
