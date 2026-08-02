f0faa9063c198b7daeaadc9fb67d2f82b893e783
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-08-02 18:34:28 +0530

    Time: 7 ms (57.23%), Space: 21.1 MB (84.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        dictData = {}
-        for i in range(len(nums)):
-            if nums[i] not in dictData.keys():
-                dictData[nums[i]] = 1
+        # dictData = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in dictData.keys():
+        #         dictData[nums[i]] = 1
+        #     else:
+        #         dictData[nums[i]] +=1
+        # return max(dictData,key=dictData.get) 
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            # When count is 0, we pick the current number as the new candidate
+            if count == 0:
+                candidate = num
+            
+            # If the number matches the candidate, increment the vote; 
+            # otherwise, decrement it
+            if num == candidate:
+                count += 1
             else:
-                dictData[nums[i]] +=1
-        return max(dictData,key=dictData.get) 
+                count -= 1
+        print(count,"---",candidate)
+        return candidate
