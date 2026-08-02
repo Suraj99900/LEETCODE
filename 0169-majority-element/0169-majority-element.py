8b19032058535df3081bfb4e4d4cd748a039508c
Suraj Jaiswal <62930129+Suraj99900@users.noreply.github.com>
2026-08-02 18:52:35 +0530

    Time: 5 ms (65.14%), Space: 21 MB (84.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -8,19 +8,14 @@ class Solution:
         #     else:
         #         dictData[nums[i]] +=1
         # return max(dictData,key=dictData.get) 
-        count = 0
+        count =0 
         candidate = None
-        
-        for num in nums:
-            # When count is 0, we pick the current number as the new candidate
-            if count == 0:
-                candidate = num
-            
-            # If the number matches the candidate, increment the vote; 
-            # otherwise, decrement it
-            if num == candidate:
-                count += 1
+        for i in nums:
+            if (count == 0):
+                candidate = i 
+           
+            if(i == candidate):
+                count+=1
             else:
-                count -= 1
-        print(count,"---",candidate)
+                count-=1
         return candidate
